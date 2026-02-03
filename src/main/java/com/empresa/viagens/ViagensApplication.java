4440c6dbba84a0cc794214c8e1dafdeffbc4460b
felipe <felipe@local>
2026-02-03 01:59:48 -0400

    test: add PerformanceBenchmarksTest and remove non-standard test file

--- src/main/java/com/empresa/viagens/ViagensApplication.java
+++ src/main/java/com/empresa/viagens/ViagensApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ViagensApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ViagensApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ViagensApplication.class, args);
+  }
 }
--- src/test/java/com/empresa/viagens/PerformanceBenchmarksTest.java
+++ src/test/java/com/empresa/viagens/PerformanceBenchmarksTest.java
@@ -2,7 +2,7 @@ package com.empresa.viagens;
 
 import org.junit.jupiter.api.Test;
 
-public class PerformanceBenchmarks {
+public class PerformanceBenchmarksTest {
 
   @Test
   void stringConcatVsBuilder() {
