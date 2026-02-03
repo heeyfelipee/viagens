64ca0a30ea6eb824ae5fa967fb4cd83ffc22b0ed
felipe <felipe@local>
2026-02-03 00:51:54 -0400

    chore: initial commit before Java 21 upgrade

---
+++ src/test/java/com/empresa/viagens/ViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.viagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
